aafff1006112a572d4a406a28b9749689676425f
rtkbala <balasathya16@yahoo.com>
2023-01-29 17:11:54 -0500

    events & handlers

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,33 @@
 var selectedChat = "general";
 
+class Event { 
+    constructor(type, payload){
+        this.type = type;
+        this.payload = payload;
+    }
+}
+
+
+function routeEvent(event) {
+    if(event.type === undefined){
+        alert('no type field in the event');
+    }
+
+    switch(event.type) {
+        case "new message":
+            console.log("new message received");
+            break;
+            default:
+                alert("unsupported message"); 
+                break;
+    }
+}
+
+function sendEvent(eventName, payload) {
+    const event = new Event(eventName, payload);
+    conn.send(JSON.stringify(event));
+}
+
 function changeChatRoom() {
     var newChat = document.getElementById("chatroom");
     if(newChat != null && newChat.value != selectedChat){
@@ -12,6 +40,7 @@ function sendMessage() {
     var newMessage = document.getElementById("message");
     if(newMessage != null){
         conn.send(newMessage.value);
+        sendEvent("send_message", newMessage.value)
     }
     return false; 
 }
@@ -26,7 +55,10 @@ window.onload = function() {
         conn = new WebSocket("ws://" + document.location.host + "/ws");
 
         conn.onmessage = function(evt) {
-            console.log(evt); 
+            const eventData = JSON.parse(evt.data);
+            const event = Object.assign(new Event, eventData);
+            
+            routeEvent(event); 
         }
     }
 
