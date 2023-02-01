22924bb4acf1c061c2a0c8923e854fbfc85bba6c
rtkbala <balasathya16@yahoo.com>
2023-01-31 23:57:07 -0500

    reset according to tutorial again, need to reviit js part

--- client.go
+++ client.go
@@ -7,17 +7,21 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// ClientList is a map used to help manage a map of clients
 type ClientList map[*Client]bool
 
+// Client is a websocket client, basically a frontend visitor
 type Client struct {
+	// the websocket connection
 	connection *websocket.Conn
-	manager    *Manager
 
-	// 	prevent concurrent writes on the websocket connection
-
+	// manager is the manager used to manage the client
+	manager *Manager
+	// egress is used to avoid concurrent writes on the WebSocket
 	egress chan Event
 }
 
+// NewClient is used to initialize a new Client with all required values initialized
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
@@ -26,65 +30,73 @@ func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	}
 }
 
-// need to revisit errors in marshalling
-
+// readMessages will start the client to read messages and handle them
+// appropriatly.
+// This is suppose to be ran as a goroutine
 func (c *Client) readMessages() {
 	defer func() {
-		// cleanup connection
+		// Graceful Close the Connection once this
+		// function is done
 		c.manager.removeClient(c)
-
 	}()
+	// Loop Forever
 	for {
+		// ReadMessage is used to read the next message in queue
+		// in the connection
 		_, payload, err := c.connection.ReadMessage()
 
 		if err != nil {
+			// If Connection is closed, we will Recieve an error here
+			// We only want to log Strange errors, but simple Disconnection
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-
 				log.Printf("error reading message: %v", err)
 			}
-			break
+			break // Break the loop to close conn & Cleanup
 		}
-
-		// errors here. expecting json but receiving text
+		// Marshal incoming data into a Event struct
 		var request Event
-
 		if err := json.Unmarshal(payload, &request); err != nil {
 			log.Printf("error marshalling message: %v", err)
 			break // Breaking the connection here might be harsh xD
 		}
 		// Route the Event
 		if err := c.manager.routeEvent(request, c); err != nil {
-			log.Println("Error handling message: ", err)
+			log.Println("Error handeling Message: ", err)
 		}
 	}
 }
 
+// writeMessages is a process that listens for new messages to output to the Client
 func (c *Client) writeMessages() {
 	defer func() {
+		// Graceful close if this triggers a closing
 		c.manager.removeClient(c)
 	}()
 
 	for {
 		select {
 		case message, ok := <-c.egress:
+			// Ok will be false Incase the egress channel is closed
 			if !ok {
+				// Manager has closed this connection channel, so communicate that to frontend
 				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					// Log that the connection is closed and the reason
 					log.Println("connection closed: ", err)
 				}
+				// Return to close the goroutine
 				return
 			}
-
 			data, err := json.Marshal(message)
-
 			if err != nil {
 				log.Println(err)
-				return
+				return // closes the connection, should we really
 			}
-
+			// Write a Regular text message to the connection
 			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
-				log.Printf("Failed to send message: %v", err)
+				log.Println(err)
 			}
-			log.Println("message sent")
+			log.Println("sent message")
 		}
+
 	}
 }
